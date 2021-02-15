3f22bd831867f951010c63eb7955272f00f03601
Mark Chenpingling <chenpingling@gmail.com>
2021-02-15 14:57:07 +0800

    add ref

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,9 @@ import Navigation from "./Navigation";
 const Header = ({ history, handleSubmit }) => {
   return (
     <div>
-      <h1>SnapShot  這是演練有改動了代碼 by Mark 2021-02-15</h1>
+      <h1>SnapShot </h1>
+      <p>這是演練有改動了代碼 by Mark 2021-02-15</p>
+      <p>根據這教程 https://app.pluralsight.com/guides/how-to-host-your-static-webpages-on-github-pages</p>
       <Form history={history} handleSubmit={handleSubmit} />
       <Navigation />
     </div>
